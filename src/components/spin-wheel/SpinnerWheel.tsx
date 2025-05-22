44aa93f506f86d97460c6dd63368c3a546b25b83
lilbouihi0 <yb1061281@gmail.com>
2025-05-22 02:38:50 +0100

    Fix: Update center button to display an icon instead of text

--- src/components/spin-wheel/SpinnerWheel.tsx
+++ src/components/spin-wheel/SpinnerWheel.tsx
@@ -98,7 +98,7 @@ const SpinnerWheel: React.FC<SpinnerWheelProps> = ({
         </svg>
       </div>
       
-      {/* Center button */}
+      {/* Center button - No text */}
       <button 
         onClick={onSpin}
         disabled={spinDisabled}
@@ -109,7 +109,11 @@ const SpinnerWheel: React.FC<SpinnerWheelProps> = ({
           transition-transform hover:scale-105 cursor-pointer animate-pulse
           disabled:opacity-50 disabled:cursor-not-allowed disabled:animate-none"
       >
-        {spinText}
+        {/* No text in button - just an icon */}
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
       </button>
       
       {/* Center circle */}
